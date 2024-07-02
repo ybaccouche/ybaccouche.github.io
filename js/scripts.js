f6fef9c11f26d68bf26228df7b5c3a572d86467c
Ryan Ott <ott.r21@gmail.com>
2024-07-02 21:27:18 +0200

    More responsiveness + sidebar + toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
+function toggleSidebar() {
+    document.querySelector('.sidebar').classList.toggle('active');
+    document.querySelector('.container').classList.toggle('sidebar-active');
+}
+
 document.getElementById('contactForm').addEventListener('submit', function (event) {
     event.preventDefault();
     alert('Thank you for your message!');
 });
+
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString();
